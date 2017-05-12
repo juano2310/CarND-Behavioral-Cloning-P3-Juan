6f2038bc452c4a1ee374fb4156ab09bc23f9ead0
juano2310 <juano23@gmail.com>
2017-05-12 15:54:25 -0400

    First revision

--- model.py
+++ model.py
@@ -17,6 +17,14 @@ print("Number of validation samples: ",len(validation_samples))
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras import backend as K
+from keras.models import Model, Sequential
+from keras.layers import Dense, GlobalAveragePooling2D, Conv2D, MaxPooling2D, Lambda, Cropping2D
+from keras.layers.core import Flatten, Dense, Dropout, SpatialDropout2D
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -32,12 +40,20 @@ def generator(samples, batch_size=32):
             	left_angle = float(batch_sample[3]) + 0.15
             	images.append(left_image)
             	angles.append(left_angle)
+            	flip_left_image = np.fliplr(left_image)
+            	flip_left_angle = -1 * left_angle
+            	images.append(flip_left_image)
+            	angles.append(flip_left_angle)
             	path = os.path.normpath(batch_sample[2]).split(os.path.sep) #name = './data2/IMG/'+path[0].split('\\')[-1]
             	name = './data3/IMG/'+path[0].split('\\')[-1] # name = './data2/IMG/'+batch_sample[0].split('/')[-1]
             	right_image = cv2.imread(name)
             	right_angle = float(batch_sample[3]) - 0.15
             	images.append(right_image)
             	angles.append(right_angle)
+            	flip_right_image = np.fliplr(right_image)
+            	flip_right_angle = -1 * right_angle
+            	images.append(flip_right_image)
+            	angles.append(flip_right_angle)
 
             X_train = np.array(images)
             y_train = np.array(angles)
@@ -45,18 +61,16 @@ def generator(samples, batch_size=32):
             yield sklearn.utils.shuffle(X_train, y_train)
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+batch_size_value = 32
+n_epoch = 15
 
-from keras.models import Model, Sequential
-from keras.layers import Dense, GlobalAveragePooling2D, Conv2D, MaxPooling2D, Lambda, Cropping2D
-from keras.layers.core import Flatten, Dense, Dropout
-from keras.optimizers import Adam
+train_generator = generator(train_samples, batch_size=batch_size_value)
+validation_generator = generator(validation_samples, batch_size=batch_size_value)
 
 model = Sequential()
 
 # trim image to only see section with road
-model.add(Cropping2D(cropping=((52,25), (10,10)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20), (0,10)), input_shape=(160,320,3)))
 
 # Preprocess incoming data, centered around zero with small standard deviation
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
@@ -65,6 +79,7 @@ model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 model.add(Conv2D(24, (5, 5), activation="relu", name="conv_1", strides=(2, 2)))
 model.add(Conv2D(36, (5, 5), activation="relu", name="conv_2", strides=(2, 2)))
 model.add(Conv2D(48, (5, 5), activation="relu", name="conv_3", strides=(2, 2)))
+model.add(SpatialDropout2D(.5, dim_ordering='default'))
 
 model.add(Conv2D(64, (3, 3), activation="relu", name="conv_4", strides=(1, 1)))
 model.add(Conv2D(64, (3, 3), activation="relu", name="conv_5", strides=(1, 1)))
@@ -72,11 +87,42 @@ model.add(Conv2D(64, (3, 3), activation="relu", name="conv_5", strides=(1, 1)))
 model.add(Flatten())
 
 model.add(Dense(1164))
+model.add(Dropout(.5))
 model.add(Dense(100, activation='relu'))
+model.add(Dropout(.5))
 model.add(Dense(50, activation='relu'))
+model.add(Dropout(.5))
 model.add(Dense(10, activation='relu'))
+model.add(Dropout(.5))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, steps_per_epoch=(len(train_samples) / 32), validation_data=validation_generator, validation_steps=(len(validation_samples)/32), epochs=10) #, verbose =2)
+
+# checkpoint
+filepath="weights/weights-improvement-{epoch:02d}-{val_loss:.2f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='auto', period=1)
+callbacks_list = [checkpoint]
+
+# Fit the model
+history_object = model.fit_generator(train_generator, steps_per_epoch=(len(train_samples) / batch_size_value), validation_data=validation_generator, validation_steps=(len(validation_samples)/batch_size_value), callbacks=callbacks_list, epochs=n_epoch)
+
+# Save model
 model.save('model.h5')
+with open('model.json', 'w') as output_json:
+    output_json.write(model.to_json())
+
+# Save TensorFlow model
+tf.train.write_graph(K.get_session().graph.as_graph_def(), logdir='.', name='model.pb', as_text=False)
+
+# Plot the training and validation loss for each epoch
+print('Generating loss chart...')
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('model.png')
+
+# Done
+print('Done.')
--- readme.md
+++ readme.md
@@ -15,7 +15,6 @@ Please see the  [rubric points](https://review.udacity.com/#!/rubrics/432/view) 
 ## Getting started
 
 The project includes the following files:
-* this README.md, [this article](https://medium.com/@ksakmann/behavioral-cloning-make-a-car-drive-like-yourself-dc6021152713) and image_transformation_pipeline.ipynb for explanation.
 * model.py containing the script to create and train the model
 * drive.py for driving the car in autonomous mode
 * model.h5 containing a trained convolution neural network
@@ -25,8 +24,18 @@ The project includes the following files:
 
 
 ## General considerations
-The simulated car is equipped with three cameras, one to the left, one in the center and one to the right of the driver that provide images from these different view points. The training track has sharp corners, exits, entries, bridges, partially missing lane lines and changing light conditions. The model developed here was trained exclusively on the training track and completes the test track.
+The simulated car is equipped with three cameras, one to the left, one in the center and one to the right of the driver that provide images from these different view points. The training track has sharp corners, exits, entries and bridges. The model developed here was trained exclusively on the training track and completes the test track.
 
+
+## Data collection
+The data collection was done on a Windows PC with a Racing Wheel. I drove around the track 2 times one one backwards. I had to adjust the path while importing the images because windows uses "\" instead of "/".
+
+## Data manipulation
+For this project I cropped 50 pixels from the top and 20 from the bottom of the images so that the model wasn't distracted by irrelevant information.
+![img](images/crop.png)
+
+Also, to make sure that I had enough information to make the right turn I augmented the data by flipping the image horizontally.
+![img](images/flip.png)
 
 ## Model architecture
 From the beginning I decided to use [CNN architecture of NVIDIA](https://arxiv.org/pdf/1604.07316v1.pdf)
@@ -36,9 +45,12 @@ From the beginning I decided to use [CNN architecture of NVIDIA](https://arxiv.o
 ## Epochs and Validation
 I shuffled the samples
 For validation purposes 20% of the training data was held back.
-After few epochs (~10) the validation and training loss settle.
+After few epochs (15) the validation and training loss settle.
 I used an Adam optimizer for training. All training was performed at the fastest graphics setting.
+After the first review of the project I added ModelCheckpoint() to save after each epoch only when there is progress.
+I also generated a graph to visualize the output.
 
+![img](images/model.png)
 
 ## Training
 The model was trained on:
@@ -55,9 +67,11 @@ Interface: USB
 Software:
 OS: Ubuntu 15.04
 Keras Version: 2.0.3
-TensorFlow version: 1.0.
+TensorFlow version: 1.0.1
 
-I rushed to implement the NVIDIA model right away. I had lot of dropouts and complexity and the car wasn't doing much. Then I started removing most of the model and after a couple it tries it looked like it was finally doing something.
+I use my own environment for testing and soon I realized that I installed Keras 2 and most of the examples were using Keras 1 so I had to update them to work on my machine.
+
+Soon after, I rushed to implement the NVIDIA model right away. I had lot of dropouts and complexity and the car wasn't doing much. Then I started removing most of the model and after a couple it tries it looked like it was finally doing something.
 
 However, it was not enough to complete the test track.
 
@@ -69,6 +83,11 @@ alt="AI cheating and taking a shortcut in the Udacity simulator " width="240" he
 My next step was to include left and right images and adjust the angle by 0.15. This output a more robust model.
 
 Finally my only problem was the right turn at the end so I tried to flip the images but this created more issues with other parts of the track so I collected more data by driving the track backwards. This really helped and I was able to finally drive the whole track without issues, even changing speed on drive.py.
+
+My reviewer point me to a big problem where I trained the model in BGR format images and my drive.py was using RGB. Changing drive.py to use images in BGR format after reading it allowed me to play a little more with the model to make further improvement.
+
+This made my model more robust and now I was able to use Dropout and even SpatialDropout2D also suggested by the reviewer.
+
 
 ## Results
 After playing and fine tuning the model I was able to let the model run around the track with no issues at variable speed.
